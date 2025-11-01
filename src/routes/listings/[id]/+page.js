622505d159bc9c492aff389fe4a788250abfea5d
Alyaman Massarani <alyaman.maasarani@gmail.com>
2025-11-02 00:45:26 +0200

    Delete duplicate HouseListing type

--- src/routes/listings/[id]/+page.js
+++ src/routes/listings/[id]/+page.js
@@ -4,27 +4,6 @@ import { error } from '@sveltejs/kit';
 // Disable prerendering for dynamic route
 export const prerender = false;
 
-/**
- * @typedef {Object} HouseListing
- * @property {number} id
- * @property {string} created_at
- * @property {string} town
- * @property {number} rent_per_month
- * @property {string} author
- * @property {number} size_m2
- * @property {number} num_bedrooms
- * @property {number} num_bathrooms
- * @property {string} address
- * @property {string|null} source_url
- * @property {string|null} phone_number
- * @property {boolean} whatsapp_comm
- * @property {boolean} rent_in_usd
- * @property {Array<{pic_name: string}>} listing_to_pic
- * @property {Object} users
- * @property {string} users.first_name
- * @property {string} users.last_name
- */
-
 /** @type {import('./$types').PageLoad} */
 export async function load({ params }) {
 	const { data: listing, error: fetchError } = await supabase

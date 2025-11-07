0f0d3fcce1a51d0dd8970ba1c384e6409ef8f47a
Alyaman Massarani <alyaman.maasarani@gmail.com>
2025-11-07 04:41:52 +0200

    Implement filtering listings with supabase in +page.js load

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -4,9 +4,17 @@ import { supabase } from '$lib/supabaseClient.js';
 export const prerender = true;
 
 /** @type {import('./$types').PageLoad} */
-export const load = async () => {
+export const load = async ({ url }) => {
+	let minPrice = url.searchParams.get('minPrice');
+	let maxPrice = url.searchParams.get('maxPrice');
+	let town = url.searchParams.get('town');
+	let numBedrooms = url.searchParams.get('numBedrooms');
+	let numBathrooms = url.searchParams.get('numBathrooms');
+
+	let townVal = url.searchParams.get('town');
+	townVal ? (town = townVal) : null;
 	// Fetch all house listings with their images
-	const listingsPromise = supabase
+	let listingsPromise = supabase
 		.from('house_listings')
 		.select(
 			`
@@ -17,6 +25,23 @@ export const load = async () => {
     `
 		)
 		.order('created_at', { ascending: false });
+
+	// Filter the query by number of bedrooms and bathrooms if parameters are provided
+	if (numBedrooms !== null) {
+		listingsPromise = listingsPromise.eq('num_bedrooms', parseInt(numBedrooms));
+	}
+	if (numBathrooms !== null) {
+		listingsPromise = listingsPromise.eq('num_bathrooms', parseInt(numBathrooms));
+	}
+	if (town !== null) {
+		listingsPromise = listingsPromise.eq('town', town);
+	}
+	if (minPrice !== null) {
+		listingsPromise = listingsPromise.gte('rent_per_month', parseInt(minPrice));
+	}
+	if (maxPrice !== null) {
+		listingsPromise = listingsPromise.lte('rent_per_month', parseInt(maxPrice));
+	}
 
 	return {
 		listings: listingsPromise
